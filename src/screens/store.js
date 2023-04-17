2a75d3c364bc7527da901be7d485f62831dd5662
usama1225 <mu75591@gmail.com>
2023-04-17 12:30:00 +0500

    store working

--- src/screens/store.js
+++ src/screens/store.js
@@ -1,63 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
-
-
+import { View, Text } from 'react-native'
+import React from 'react'
 
 const Store = () => {
-  const [location, setLocation] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-      }
-  
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
   return (
-    <View style={styles.container}>
-      {location ? (
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          <Marker
-            coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-            }}
-            title="You are here"
-          />
-        </MapView>
-      ) : (
-        <Text>Loading...</Text>
-      )}
+    <View>
+      <Text>Store</Text>
     </View>
-  );
+  )
 }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    map: {
-      width: '100%',
-      height: '100%',
-    },
-  });
-  
 
-
-export default Store;
+export default Store
