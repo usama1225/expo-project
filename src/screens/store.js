12f25e7a67eff2e047ab3b2a1a02ab79970bf633
usama1225 <mu75591@gmail.com>
2023-06-13 17:01:41 +0500

    hi

--- src/screens/store.js
+++ src/screens/store.js
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Store = () => {
   return (
-    <View>
-      <Text>Store</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Store</Text>
+      <View style={styles.page}>
+      
     </View>
+    </View>
+  
   )
 }
 
+const styles= StyleSheet.create({
+  title:{
+    color: 'red',
+    textAlign: 'center',
+    padding: '20',
+    
+  },
+  page:{
+    height:'70',
+    backgroundColor: 'orange',
+    padding: 20,
+    margin: '20',
+  }
+});
 export default Store
